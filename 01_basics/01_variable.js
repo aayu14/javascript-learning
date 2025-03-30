bb903ecdce152553a9eb4f56a5b5baa6bc47da50
Ayush <am94947651@gmail.com>
2025-03-30 06:05:18 +0000

    learninf from scratch

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,18 @@
+const accountId=14563212  //user cant chnages const variable if it assign 
+let username="ayush1404"   // let is a scope level variable define as we can easily use same variable at diffrenr scope and their value would be diffrent
+var password="aayu1404"   // var can also use but we now limitig the uses as once the var value chnage it effect every where in code
+city="mumbai"  // we have define var /let instead on not defining
+
+let name; // when we not define value then console say the value is undefine
+
+
+username="test10404"
+password="test1404"
+city="madhubani"
+
+
+console.table(
+    [accountId,username,password,city,name]
+
+);
+
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,24 @@
+//alert("Hello, Aayu") // we using node js not browser , we have diffrent approach to run alert
+
+let age = 24
+let name ="ayush"
+let city=null
+let state
+
+//primitive 
+
+// number => 2**53 range
+//bigint
+//string  => ""
+//boolean => true/false
+// undefine
+//null => standalone value
+//symbol => unique
+
+  
+
+console.table([name,age,city])
+console.log(typeof city)
+console.log(typeof state)
+
+//object
